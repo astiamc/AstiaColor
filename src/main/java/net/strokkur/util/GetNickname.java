ea2b36581ebbccb2557f53582407a0872901f6c7
Strokkur24 <strokkur.24@gmail.com>
2024-03-09 11:14:30 +0100

    Added a ~ before nicked players

--- src/main/java/net/strokkur/util/GetNickname.java
+++ src/main/java/net/strokkur/util/GetNickname.java
@@ -7,7 +7,10 @@ import org.bukkit.entity.Player;
 public class GetNickname {
 
     public static String get(Player p) {
-        return ChatColor.stripColor(PlaceholderAPI.setPlaceholders(p, "%essentials_nickname_stripped%"));
+        String stripped = ChatColor.stripColor(PlaceholderAPI.setPlaceholders(p, "%essentials_nickname_stripped%"));
+        if (stripped.equals(p.getName()))
+            return stripped;
+        return "~" + stripped;
     }
 
 }
